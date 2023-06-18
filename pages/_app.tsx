de30f0c4b7997ba72aeff663a7e61464f38fa4a6
skyglass <skyglass2001@gmail.com>
2023-06-18 22:18:57 +0200

    try to fix github pipeline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,42 @@
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import { ReactElement, ReactNode } from 'react';
 
+import { API_MOCKING } from '@/config/constants';
+import { MSWWrapperProps } from '@/lib/msw';
 import { AppProvider } from '@/providers/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MSWWrapper = dynamic<MSWWrapperProps>(() =>
+  import('@/lib/msw').then(({ MSWWrapper }) => MSWWrapper)
+);
+
+const App = ({
+  Component,
+  pageProps,
+}: AppPropsWithLayout) => {
+  const getLayout =
+    Component.getLayout ?? ((page) => page);
+
+  const pageContent = getLayout(
+    <Component {...pageProps} />
+  );
+
   return (
     <AppProvider>
-      <Component {...pageProps} />
+      {API_MOCKING ? (
+        <MSWWrapper>{pageContent}</MSWWrapper>
+      ) : (
+        pageContent
+      )}
     </AppProvider>
   );
 };
